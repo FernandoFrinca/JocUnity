a2d1e9ec21c79f6e2de9d3de2c7544e7a1394cc6
iankooo <ianko_radoslav@yahoo.com>
2021-07-13 17:51:17 +0300

    proiect adaugat

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Joystick joystick;
+    public Animator animator;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+    
+    void Update()
+    {
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+        if (joystick.Horizontal>=1.2f)
+        {
+            horizontalMove = runSpeed;
+        }
+        else if(joystick.Horizontal <=1.2f)
+        {
+            horizontalMove = -runSpeed;
+        }
+        else
+        {
+            horizontalMove = 0;
+        }
+
+        float verticalMove = joystick.Vertical;
+        horizontalMove = joystick.Horizontal*runSpeed;
+        if(verticalMove>=.5f)
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+        if(verticalMove<=-.5f)
+        {
+            crouch = true;
+        }else
+        {
+            crouch = false;
+        }
+    }
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouch", isCrouching);
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove*Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+
+
+}
